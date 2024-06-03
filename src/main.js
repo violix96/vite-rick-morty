ba1a21ece23a10f5747c1e3bc3fcfc37d708883c
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-03 15:33:40 +0200

    [UPD] installed roboto font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createApp } from 'vue';
 //import style
 import './assets/scss/main.scss';
 
+// import roboto font
+import "@fontsource/roboto";
+
 //import vue root component
 import App from './App.vue';
 
