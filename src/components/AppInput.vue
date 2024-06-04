3c06e476c3e4a579f67b9ea7e6775cc3f9f7370b
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-04 23:44:40 +0200

    [UPD] fix reset button

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -14,7 +14,7 @@ export default {
         <div class="container text-center">
             <input v-model="inputText" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button @click="$emit('searchEvent', inputText)" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            <button @click="$emit('resetEvent')" class="btn btn-outline-danger my-2 my-sm-0" type="submit">Reset</button>
+            <button @click="$emit('resetEvent'); inputText= '' " class="btn btn-outline-danger my-2 my-sm-0" type="submit">Reset</button>
         </div>
     </main>
 </template>
